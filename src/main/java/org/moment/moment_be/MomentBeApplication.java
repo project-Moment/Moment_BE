e0a5f0b56a9d6c2ab33568a11902c220268e973b
rlawotmd <kimjs2400@naver.com>
2024-05-24 00:22:39 +0900

    project init

---
+++ src/main/java/org/moment/moment_be/MomentBeApplication.java
@@ -0,0 +1,13 @@
+package org.moment.moment_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/moment/moment_be/MomentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moment.moment_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

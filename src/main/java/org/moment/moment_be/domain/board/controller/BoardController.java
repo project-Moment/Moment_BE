dc7c7b839bab7c594f0876962424b66fb6f6d9e6
rlawotmd <kimjs2400@naver.com>
2024-05-25 02:03:18 +0900

    게시물 선택 시 조회 기능

--- src/main/java/org/moment/moment_be/domain/board/controller/BoardController.java
+++ src/main/java/org/moment/moment_be/domain/board/controller/BoardController.java
@@ -5,12 +5,14 @@ import org.moment.moment_be.domain.board.entity.Post;
 import org.moment.moment_be.domain.board.service.BoardReadService;
 import org.moment.moment_be.domain.board.service.BoardWriteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 @RequestMapping("/board")
 @RestController
@@ -29,6 +31,19 @@ public class BoardController {
     public ResponseEntity<Post> createPost(@RequestBody Post post) {
         Post createdPost = boardWriteService.createPost(post);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+    }
+
+    /*@GetMapping("")
+    public ResponseEntity<Page<Post>> getPosts(Pageable pageable) {
+        Page<Post> postsPage = boardReadService.getPosts(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(postsPage);
+    }*/
+
+    @GetMapping("/{post_id}")
+    public ResponseEntity<Post> getPost(@PathVariable Long post_id) {
+        Post post = boardReadService.getPostById(post_id);
+        if (post != null) return ResponseEntity.ok().body(post);
+        else return ResponseEntity.notFound().build();
     }
 
 
--- src/main/java/org/moment/moment_be/domain/board/service/BoardReadService.java
+++ src/main/java/org/moment/moment_be/domain/board/service/BoardReadService.java
@@ -1,8 +1,32 @@
 package org.moment.moment_be.domain.board.service;
 
+
+import org.moment.moment_be.domain.board.entity.Post;
+import org.moment.moment_be.domain.board.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BoardReadService {
+
+    private PostRepository postRepository;
+
+    @Autowired
+    public BoardReadService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Page<Post> getPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+
+    public Post getPostById(Long post_id) {
+        Optional<Post> postOptional = postRepository.findById(post_id);
+        return postOptional.orElse(null);
+    }
 
 }

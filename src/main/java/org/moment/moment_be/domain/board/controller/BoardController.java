8c1a3d84a03ab98266f8fc0d78f518397f8e7e44
rlawotmd <kimjs2400@naver.com>
2024-05-25 03:11:59 +0900

    get 카테고리 ~ing, get all ~ing

--- src/main/java/org/moment/moment_be/domain/board/controller/BoardController.java
+++ src/main/java/org/moment/moment_be/domain/board/controller/BoardController.java
@@ -1,18 +1,21 @@
 package org.moment.moment_be.domain.board.controller;
 
 
+import org.moment.moment_be.domain.board.dto.PostDto;
 import org.moment.moment_be.domain.board.entity.Post;
 import org.moment.moment_be.domain.board.service.BoardReadService;
 import org.moment.moment_be.domain.board.service.BoardWriteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
 import java.util.List;
+
 
 @RequestMapping("/board")
 @RestController
@@ -33,16 +36,25 @@ public class BoardController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
     }
 
-    /*@GetMapping("")
+    @GetMapping("")
     public ResponseEntity<Page<Post>> getPosts(Pageable pageable) {
+        pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("created_at").descending());
+
         Page<Post> postsPage = boardReadService.getPosts(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(postsPage);
-    }*/
+    }
 
     @GetMapping("/{post_id}")
-    public ResponseEntity<Post> getPost(@PathVariable Long post_id) {
+    public ResponseEntity<Post> getPostById(@PathVariable Long post_id) {
         Post post = boardReadService.getPostById(post_id);
         if (post != null) return ResponseEntity.ok().body(post);
+        else return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<PostDto>> getPostByCategory(@PathVariable String category) {
+        List<PostDto> posts = boardReadService.getPostByCategory(category);
+        if (!posts.isEmpty()) return ResponseEntity.status(HttpStatus.OK).body(posts);
         else return ResponseEntity.notFound().build();
     }
 
--- src/main/java/org/moment/moment_be/domain/board/service/BoardReadService.java
+++ src/main/java/org/moment/moment_be/domain/board/service/BoardReadService.java
@@ -1,6 +1,7 @@
 package org.moment.moment_be.domain.board.service;
 
 
+import org.moment.moment_be.domain.board.dto.PostDto;
 import org.moment.moment_be.domain.board.entity.Post;
 import org.moment.moment_be.domain.board.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class BoardReadService {
@@ -27,6 +30,19 @@ public class BoardReadService {
     public Post getPostById(Long post_id) {
         Optional<Post> postOptional = postRepository.findById(post_id);
         return postOptional.orElse(null);
+    }
+
+    public List<PostDto> getPostByCategory(String category) {
+        List<Post> posts = postRepository.findByCategory(category);
+        return posts.stream().map(post -> {
+            PostDto postDto = new PostDto();
+            postDto.setPost_id(post.getPost_id());
+            postDto.setStudent_id(post.getStudent_id());
+            postDto.setCategory(post.getCategory());
+            postDto.setTitle(post.getTitle());
+            postDto.setCreated_at(post.getCreated_at());
+            return postDto;
+        }).collect(Collectors.toList());
     }
 
 }

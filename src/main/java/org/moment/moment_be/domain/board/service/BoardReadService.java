4268c82fab69f7dab82aafce17f6fda6e0abb1e5
rlawotmd <kimjs2400@naver.com>
2024-05-25 22:30:46 +0900

    게시물 역순 조회

--- src/main/java/org/moment/moment_be/domain/board/service/BoardReadService.java
+++ src/main/java/org/moment/moment_be/domain/board/service/BoardReadService.java
@@ -5,8 +5,7 @@ import org.moment.moment_be.domain.board.dto.PostDto;
 import org.moment.moment_be.domain.board.entity.Post;
 import org.moment.moment_be.domain.board.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,26 +22,29 @@ public class BoardReadService {
         this.postRepository = postRepository;
     }
 
-    public Page<Post> getPosts(Pageable pageable) {
-        return postRepository.findAll(pageable);
+    public List<PostDto> getPosts() {
+        List<Post> posts = postRepository.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
+        return posts.stream().map(this::converToDto).collect(Collectors.toList());
     }
 
-    public Post getPostById(Long post_id) {
-        Optional<Post> postOptional = postRepository.findById(post_id);
+    public Post getPostById(Long postId) {
+        Optional<Post> postOptional = postRepository.findById(postId);
         return postOptional.orElse(null);
     }
 
     public List<PostDto> getPostByCategory(String category) {
-        List<Post> posts = postRepository.findByCategory(category);
-        return posts.stream().map(post -> {
-            PostDto postDto = new PostDto();
-            postDto.setPost_id(post.getPost_id());
-            postDto.setStudent_id(post.getStudent_id());
-            postDto.setCategory(post.getCategory());
-            postDto.setTitle(post.getTitle());
-            postDto.setCreated_at(post.getCreated_at());
-            return postDto;
-        }).collect(Collectors.toList());
+        List<Post> posts = postRepository.findByCategory(category, Sort.by("createdAt").descending());
+        return posts.stream().map(this::converToDto).collect(Collectors.toList());
+    }
+
+    private PostDto converToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setPostId(post.getPostId());
+        postDto.setStudentId(post.getStudentId());
+        postDto.setCategory(post.getCategory());
+        postDto.setTitle(post.getTitle());
+        postDto.setCreatedAt(post.getCreatedAt());
+        return postDto;
     }
 
 }

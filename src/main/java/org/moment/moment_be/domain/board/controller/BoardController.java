6c4251e0d7d04f6072c2f867081d86d4cadc1332
rlawotmd <kimjs2400@naver.com>
2024-05-25 22:38:12 +0900

    final 선언

--- src/main/java/org/moment/moment_be/domain/board/controller/BoardController.java
+++ src/main/java/org/moment/moment_be/domain/board/controller/BoardController.java
@@ -6,10 +6,6 @@ import org.moment.moment_be.domain.board.entity.Post;
 import org.moment.moment_be.domain.board.service.BoardReadService;
 import org.moment.moment_be.domain.board.service.BoardWriteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +17,8 @@ import java.util.List;
 @RestController
 public class BoardController {
 
-    private BoardWriteService boardWriteService;
-    private BoardReadService boardReadService;
+    private final BoardWriteService boardWriteService;
+    private final BoardReadService boardReadService;
 
     @Autowired
     public BoardController(BoardWriteService boardWriteService, BoardReadService boardReadService) {

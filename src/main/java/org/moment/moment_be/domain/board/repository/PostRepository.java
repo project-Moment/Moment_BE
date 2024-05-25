64d5fc217fecda107ce3dd88cd90bee44fbbebb9
rlawotmd <kimjs2400@naver.com>
2024-05-25 22:29:36 +0900

    get by 카테고리 레파지토리 수정

--- src/main/java/org/moment/moment_be/domain/board/repository/PostRepository.java
+++ src/main/java/org/moment/moment_be/domain/board/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package org.moment.moment_be.domain.board.repository;
 
 import org.moment.moment_be.domain.board.entity.Post;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByCategory(String category);
+    List<Post> findByCategory(String category, Sort sort);
 }

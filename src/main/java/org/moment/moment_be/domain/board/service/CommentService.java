3c8ef82471785af9c20dd2fb95dc53031d15cb18
rlawotmd <kimjs2400@naver.com>
2024-05-27 11:47:12 +0900

    대댓글에 대한 서비스 추가

--- src/main/java/org/moment/moment_be/domain/board/service/CommentService.java
+++ src/main/java/org/moment/moment_be/domain/board/service/CommentService.java
@@ -1,17 +1,14 @@
 package org.moment.moment_be.domain.board.service;
 
+import org.moment.moment_be.domain.board.dto.CommentDto;
+import org.moment.moment_be.domain.board.dto.reCommentDto;
 import org.moment.moment_be.domain.board.entity.Comment;
-import org.moment.moment_be.domain.board.entity.Post;
 import org.moment.moment_be.domain.board.repository.CommentRepository;
 import org.moment.moment_be.domain.board.repository.PostRepository;
-import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -31,4 +28,37 @@ public class CommentService {
     public Comment createComment(Comment comment) {
         return commentRepository.save(comment);
     }
+
+    public List<CommentDto> getCommentsByPostId(Long postId) {
+        List<Comment> comments = commentRepository.findByPostIdAndParentCommentIdIsNull(postId);
+        return comments.stream()
+                .map(this::convertToCommentDto)
+                .collect(Collectors.toList());
+    }
+
+    private CommentDto convertToCommentDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setCommentId(comment.getCommentId());
+        commentDto.setStudentId(comment.getStudentId());
+        commentDto.setContent(comment.getContent());
+        commentDto.setCreatedAt(comment.getCreatedAt());
+
+        List<Comment> reComments = commentRepository.findByParentCommentId(comment.getCommentId());
+        List<reCommentDto> reCommentDto = reComments.stream()
+                .map(this::convertToReCommentDto)
+                .collect(Collectors.toList());
+
+        commentDto.setRecomment(reCommentDto);
+        return commentDto;
+    }
+
+    private reCommentDto convertToReCommentDto(Comment comment) {
+        reCommentDto reCommentDto = new reCommentDto();
+        reCommentDto.setCommentId(comment.getCommentId());
+        reCommentDto.setStudentId(comment.getStudentId());
+        reCommentDto.setContent(comment.getContent());
+        reCommentDto.setCreatedAt(comment.getCreatedAt());
+        return reCommentDto;
+    }
+
 }

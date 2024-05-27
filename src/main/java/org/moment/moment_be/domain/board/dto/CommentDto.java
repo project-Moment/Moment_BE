942ab867e2dd28369fc6a98139404a924cc33565
rlawotmd <kimjs2400@naver.com>
2024-05-27 11:47:36 +0900

    댓글 Dto 구조 변경

--- src/main/java/org/moment/moment_be/domain/board/dto/CommentDto.java
+++ src/main/java/org/moment/moment_be/domain/board/dto/CommentDto.java
@@ -3,12 +3,13 @@ package org.moment.moment_be.domain.board.dto;
 import lombok.Data;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 public class CommentDto {
     private Long commentId;
-    private Long parentCommentId;
     private Long studentId;
     private String content;
     private Timestamp createdAt;
+    private List<reCommentDto> recomment;
 }
